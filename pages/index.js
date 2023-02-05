880eba9da600dce6f44107761b4a6ddf95997f12
Ildar <ildar.1795@gmail.com>
2023-02-05 18:37:21 +0400

    second-commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Meta from '@/app/utils/Meta'
 import Image from 'next/image'
-import product from 'app/assets/images/product_1.webp'
 import Products from '@/app/components/Products/Products'
 export default function Home({ products }) {
 	return (
@@ -13,7 +12,9 @@ export default function Home({ products }) {
 	)
 }
 export const getStaticProps = async () => {
-	const res = await fetch('http://localhost:5000/products')
+	const res = await fetch(
+		'https://633ab4c7e02b9b64c6155e2f.mockapi.io/products'
+	)
 	const products = await res.json()
 
 	return {
--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -6,14 +6,18 @@ const Info = ({ product }) => {
 	return <ProductInfo product={product} />
 }
 export const getStaticPaths = async () => {
-	const res = await fetch('http://localhost:5000/products')
+	const res = await fetch(
+		'https://633ab4c7e02b9b64c6155e2f.mockapi.io/products'
+	)
 	const data = await res.json()
 	const paths = data.map(p => ({ params: { id: p.id.toString() } }))
 	return { paths, fallback: 'blocking' }
 }
 
 export const getStaticProps = async ({ params }) => {
-	const res = await fetch(`http://localhost:5000/products/${params.id}`)
+	const res = await fetch(
+		`https://633ab4c7e02b9b64c6155e2f.mockapi.io/products/${params.id}`
+	)
 	const data = await res.json()
 	return {
 		props: {
